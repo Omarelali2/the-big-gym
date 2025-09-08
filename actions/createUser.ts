411eb6eadbd636f1c3b0915d8fffce9afdd3920e
Omarelali2 <162860381+Omarelali2@users.noreply.github.com>
2025-09-08 07:49:37 +0300

    jj

--- actions/createUser.ts
+++ actions/createUser.ts
@@ -11,42 +11,31 @@ export async function createUserAction({
   clerkUserId: string;
   email: string;
   name?: string;
-  username: string; 
+  username: string;
   imageUrl?: string;
   packageId?: number;
 }) {
   try {
-    const existingUser = await db.user.findUnique({
+    const user = await db.user.upsert({
       where: { clerkUserId },
+      update: {
+        name,
+        username,
+        imageUrl,
+        selectedPackageId: packageId ?? undefined,
+        subscriptionActive: packageId ? true : false,
+      },
+      create: {
+        clerkUserId,
+        email,
+        name,
+        username,
+        imageUrl,
+        selectedPackageId: packageId ?? undefined,
+        subscriptionActive: packageId ? true : false,
+        isAdmin: email === "elaliomar30@gmail.com",
+      },
     });
-
-    let user;
-
-    if (existingUser) {
-      user = await db.user.update({
-        where: { clerkUserId },
-        data: {
-          name,
-          username,
-          imageUrl,
-          selectedPackageId: packageId ?? existingUser.selectedPackageId,
-          subscriptionActive: packageId ? true : existingUser.subscriptionActive,
-        },
-      });
-    } else {
-      user = await db.user.create({
-        data: {
-          clerkUserId,
-          email,
-          name,
-          username,
-          imageUrl,
-          selectedPackageId: packageId ?? undefined,
-          subscriptionActive: packageId ? true : false,
-          isAdmin: email === "elaliomar30@gmail.com",
-        },
-      });
-    }
 
     return user;
   } catch (error) {
